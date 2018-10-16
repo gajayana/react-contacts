3300afe5cd60d3450009e9dc13f1dc306c8e9b79
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2018-10-16 18:51:33 +0700

    Updates

---
+++ precache-manifest.a6520a3d130214fb4be6398b1894128c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bdd91ccb2d01f4ef4d0",
+    "url": "/react-contacts/static/js/runtime~main.5bdd91cc.js"
+  },
+  {
+    "revision": "33ef0a128d647879062a",
+    "url": "/react-contacts/static/js/main.33ef0a12.chunk.js"
+  },
+  {
+    "revision": "51fcbc0b57ad45f04e61",
+    "url": "/react-contacts/static/js/1.51fcbc0b.chunk.js"
+  },
+  {
+    "revision": "33ef0a128d647879062a",
+    "url": "/react-contacts/static/css/main.236a84cb.chunk.css"
+  },
+  {
+    "revision": "307fe001662826bd731844a4a77c7fe1",
+    "url": "/react-contacts/index.html"
+  }
+];
