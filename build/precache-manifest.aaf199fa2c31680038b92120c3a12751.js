a6fca62da116dcc88bc5b6676caa576f25d156f6
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2018-10-16 18:51:10 +0700

    worked on contacts list view

---
+++ build/precache-manifest.aaf199fa2c31680038b92120c3a12751.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bdd91ccb2d01f4ef4d0",
+    "url": "/react-contacts/static/js/runtime~main.5bdd91cc.js"
+  },
+  {
+    "revision": "c6bf96730ff0e7c12275",
+    "url": "/react-contacts/static/js/main.c6bf9673.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/react-contacts/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "c6bf96730ff0e7c12275",
+    "url": "/react-contacts/static/css/main.ffc04f41.chunk.css"
+  },
+  {
+    "revision": "6c7325c9469f109eb1026dc96b961d4f",
+    "url": "/react-contacts/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,27 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
+
+function handleImage(str) {
+  return (str.indexOf('http://') === 0 || str.indexOf('https://') === 0) 
+            ? 
+            str.replace('http://', 'https://') 
+            : 
+            'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
+}
+
+const Contact = ({item}) => {
+  let profileImageStyle = {
+    backgroundImage : 'url("' + handleImage(item.photo) + '")'
+  };
+
+  return(
+    <div className="App--profile-box">
+      <div className="App--profile-image" style={profileImageStyle}></div>
+      <div className="text-center">{item.firstName} {item.lastName}</div>
+      <div>
+        <button>Update</button>
+        <button>Remove</button>
+      </div>
+    </div>
+  );
+};
+
+const ContactsList = ({items}) => {
+  const persons = items.map((item) => {
+    return (<Contact item={item} key={item.id} />);
+  });
+  return (<div className="App--profiles">{ persons }</div>);
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [],
+    }
+    // No 'Access-Control-Allow-Origin' header is present.
+    this.url = 'https://cors-anywhere.herokuapp.com/https://simple-contact-crud.herokuapp.com';
+    // this.url = 'https://simple-contact-crud.herokuapp.com';
+
+    this.handleCreate = this.handleCreate.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ContactsList items={ this.state.contacts } />
+        <div><button onClick={this.handleCreate}>Create</button></div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    
+    fetch(this.url + '/contact')
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({contacts : result.data})
+      })
+      .catch(error => console.log(error))
+  }
+
+  handleCreate() {
+    // this.setState({
+    //   arrayvar: [...this.state.arrayvar, newelement]
+    // })
+    this.setState({
+      contacts: [...this.state.contacts, {
+        "id": "93ad6070-c92b-11e8-b02f-cbfa15db428c",
+        "firstName": "Bilbo",
+        "lastName": "Baggins",
+        "age": 111,
+        "photo": "http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550"
+      }]
+    })
   }
 }
 
