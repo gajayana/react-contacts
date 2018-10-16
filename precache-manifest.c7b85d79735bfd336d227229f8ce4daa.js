b6993f992979b672dc1110302d2c4149699c72fe
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2018-10-16 11:46:32 +0700

    Updates

---
+++ precache-manifest.c7b85d79735bfd336d227229f8ce4daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-contacts/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5bdd91ccb2d01f4ef4d0",
+    "url": "/react-contacts/static/js/runtime~main.5bdd91cc.js"
+  },
+  {
+    "revision": "e75e26cf9cefe8881705",
+    "url": "/react-contacts/static/js/main.e75e26cf.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-contacts/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "e75e26cf9cefe8881705",
+    "url": "/react-contacts/static/css/main.40252498.chunk.css"
+  },
+  {
+    "revision": "4b368aabdec02d97d216a7f6dd4f75fd",
+    "url": "/react-contacts/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.68d676ffb5f18247b6d9494267136b35.js"
+  "/react-contacts/precache-manifest.c7b85d79735bfd336d227229f8ce4daa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-contacts/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
